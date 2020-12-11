3723fc2239dcf9b9d4f91adc06dd6a9d32904f64
Laoraid <a99azaz@gmail.com>
2020-12-11 23:36:13 +0900

    1.0

--- BindTimer/BindTimer/Properties/AssemblyInfo.cs
+++ BindTimer/BindTimer/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // 어셈블리에 대한 일반 정보는 다음 특성 집합을 통해 
 // 제어됩니다. 어셈블리와 관련된 정보를 수정하려면
 // 이러한 특성 값을 변경하세요.
-[assembly: AssemblyTitle("WpfApp1")]
+[assembly: AssemblyTitle("BindTImer")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("WpfApp1")]
-[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("BindTimer")]
+[assembly: AssemblyCopyright("Copyright ©  2020 Laoraid")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
